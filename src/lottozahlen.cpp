8d99cea28431097c77e10c021857a60da3d6d643
Andreas Böttger <andy.boettger@gmail.com>
2015-03-09 19:23:42 +0100

    I have a window and a compiler error :\

--- src/lottozahlen.cpp
+++ src/lottozahlen.cpp
@@ -5,13 +5,21 @@
  *      Author: aboettger
  */
 
-#include <algorithm>
-#include <cstddef>
+#include <glibmm/fileutils.h>
+#include <glibmm/markup.h>
+#include <glibmm/refptr.h>
+#include <glibmm/ustring.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
 #include <iostream>
-#include <random>
-#include <vector>
 
 #include "Lotto.h"
+
+#define UI_FILE "ui/main.glade"
+
+Gtk::Window* pWindow = 0;
+Gtk::Label* pLabel = 0;
 
 int
 main (int argc, char **argv)
@@ -26,6 +34,33 @@ main (int argc, char **argv)
   std::cout << std::endl;
 
   std::cout << "Zusatzzahl: " << lotto.getZusatzzahl ();
+
+  Gtk::Main kit (argc, argv);
+  Glib::RefPtr<Gtk::Builder> refBuilder = Gtk::Builder::create ();
+  try
+    {
+      refBuilder = Gtk::Builder::create_from_file (UI_FILE);
+    }
+  catch (const Glib::FileError& ex)
+    {
+      std::cerr << "FileError: " << ex.what () << std::endl;
+      return (1);
+    }
+  catch (const Glib::MarkupError& ex)
+    {
+      std::cerr << "MarkupError: " << ex.what () << std::endl;
+      return (1);
+    }
+  catch (const Gtk::BuilderError& ex)
+    {
+      std::cerr << "BuilderError: " << ex.what () << std::endl;
+      return (1);
+    }
+
+  refBuilder->get_widget ("mainWindow", pWindow);
+  refBuilder->get_widget ("label_1", pLabel);
+
+  kit.run (*pWindow);
 
   return (0);
 }
